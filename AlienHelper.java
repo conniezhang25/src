79227bff6bd58a50e67e680958b27bc8d48b037d
conniezhang25 <36180578+conniezhang25@users.noreply.github.com>
2019-12-14 19:46:35 -0500

    Delete AlienHelper.java

--- AlienHelper.java
+++
@@ -1,8 +0,0 @@
-import javax.swing.*;
-
-public class AlienHelper {
-    public static void main(String[] args) {
-        JOptionPane.showMessageDialog(null, "Hello Earthling");
-        JOptionPane.showMessageDialog(null, "We come in peace.");
-    }
-}
