9331e1385ff51ae01b3724645f517abb95f5cbdb
conniezhang25 <36180578+conniezhang25@users.noreply.github.com>
2019-12-14 20:09:44 -0500

    Delete TestStudent.java

--- TestStudent.java
+++
@@ -1,9 +0,0 @@
-public class TestStudent
-{
- public static void main (String [] args)
- {
-    Student lau = new Student ("Edmund", 1290, 3.95);
-    System.out.println (lau.getID());
-    System.out.println (lau);
- }
-}
